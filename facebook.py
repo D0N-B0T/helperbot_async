e3d868a930f650ba60a73915ed266b0cc7db8af4
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2023-01-14 04:09:48 -0300

    asd

--- facebook.py
+++ facebook.py
@@ -13,7 +13,6 @@ TMP_FOLDER = "tmp/"
     
 async def send_facebook_video(update, context):
     await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=constants.ChatAction.UPLOAD_DOCUMENT)
-    logger.debug(f"Sending facebook video from {update.message.from_user.username} ({update.message.from_user.id})")
     
     ydl_opts = {'outtmpl': '%(id)s.%(ext)s'}
     
@@ -22,25 +21,31 @@ async def send_facebook_video(update, context):
         
     caption = f"{info['title']}"
     logger.debug(f"Video title: {info['title']}")
+    
+    
     formats = []
     for f in info['formats']:
         if f['ext'] == 'mp4':
             formats.append(f)
-            
-    logger.debug(f"Formats: {formats}")
+           
+    
     file_url = formats[-1]['url']  
     logger.debug(f"File url: {file_url}")  
     
     file_name = str(uuid.uuid4())+".mp4"
     FileDownload().download_video(file_url, TMP_FOLDER, file_name)
+    
     logger.debug(f"File name downloaded: {file_name}")
+       
     await update.message.reply_video(video="tmp/"+file_name, caption=caption[:1000], parse_mode='HTML') 
     #this is a check to see if the file is too big to be sent
-    if not await file_in_limits(file_url):
-        file_url = formats[-2]['url']
-    #if file is not too big, send it
-    if await file_in_limits(file_url):
-        await update.message.reply_video(video="tmp/"+file_name, caption=caption[:1000], parse_mode='HTML') 
-    else:
-        print("File too big")
-        await update.message.reply_html(f"El video es muy grande y no puedo subirlo, pero <a href='{file_url}'>si haces clic aca</a> lo podrás ver en el navegador. Es una mierda de solucion pero es lo que tengo para ofrecer por el momento.")
+    # if not await file_in_limits(file_url):
+    #     file_url = formats[-2]['url']
+    # #if file is not too big, send it
+    
+    
+    # if await file_in_limits(file_url):
+    #     await update.message.reply_video(video="tmp/"+file_name, caption=caption[:1000], parse_mode='HTML') 
+    # else:
+    #     print("File too big")
+    #     await update.message.reply_html(f"El video es muy grande y no puedo subirlo, pero <a href='{file_url}'>si haces clic aca</a> lo podrás ver en el navegador. Es una mierda de solucion pero es lo que tengo para ofrecer por el momento.")
