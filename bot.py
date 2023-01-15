568a6a05a169040a8ac911cc6af20201f5a28f10
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2023-01-15 20:50:50 -0300

    1.7

--- bot.py
+++ bot.py
@@ -174,17 +174,24 @@ async def link_downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_text("Por el momento no puedo bajar videos de instagram 😥")
             if "settings" not in context.chat_data or context.chat_data["settings"]["instagramp"] == "✅":
                 await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=constants.ChatAction.UPLOAD_DOCUMENT)
-                split_instagram_url = update.message.text.split("?")[0].split("/")
-                shortcode = split_instagram_url[4]
-                logger.info(f"Downloading instagram post {split_instagram_url} with shortcode {shortcode}")
-                try:
-                    #comando = 'bash yt-dlp/yt-dlp.sh '+ update.message.text +' -o '+shortcode+'.mp4'
-                    url = update.message.text[1]
-                    logger.info(f"Downloading instagram post {url}")
-                    os.system('bash yt-dlp/yt-dlp.sh '+ url)
-                except Exception as e:
-                    logger.error(f"Error downloading instagram post {split_instagram_url} with shortcode {shortcode}: {e}")
-                    return
+                #mensaje "/video https://www.instagram.com/p/"
+                url = update.message.text[1]
+                os.system('bash yt-dlp/yt-dlp.sh '+ url)
+                await update.message.reply_video(video=url, parse_mode='HTML')
+
+                
+                
+                #split_instagram_url = update.message.text.split("?")[0].split("/")
+                #shortcode = split_instagram_url[4]
+                #logger.info(f"Downloading instagram post {split_instagram_url} with shortcode {shortcode}")
+                # try:
+                #     #comando = 'bash yt-dlp/yt-dlp.sh '+ update.message.text +' -o '+shortcode+'.mp4'
+                #     url = update.message.text[1]
+                #     logger.info(f"Downloading instagram post {url}")
+                #     os.system('bash yt-dlp/yt-dlp.sh '+ url)
+                # except Exception as e:
+                #     logger.error(f"Error downloading instagram post {split_instagram_url} with shortcode {shortcode}: {e}")
+                #     return
 
                      
     
