858e4976aad163f6b337f6bc548642e2aa4a6a75
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2023-02-01 02:23:25 -0300

    asd

--- modules/instagram.py
+++ modules/instagram.py
@@ -6,16 +6,6 @@ async def send_instagram_video(update, context):
     url = update.message.text.split(" ")[1]                
     split_instagram_url = update.message.text.split("?")[0].split("/")
     
-    parte0 = split_instagram_url[0]
-    logger.info(f"Parte0: {parte0}")
-    parte11 = split_instagram_url[1]
-    logger.info(f"Parte11: {parte11}")
-    parte22 = split_instagram_url[2]
-    logger.info(f"Parte22: {parte22}")
-    parte1 = split_instagram_url[3]
-    logger.info(f"Parte1: {parte1}")
-    parte2 = split_instagram_url[4]
-    logger.info(f"Parte2: {parte2}")
     parte3 = split_instagram_url[5]
     logger.info(f"Parte3: {parte3}")
     
@@ -26,7 +16,7 @@ async def send_instagram_video(update, context):
     logger.info(f"Starting download instagram post {url}")
 
     try:
-        os.system('bash yt-dlp/yt-dlp.sh '+ url +' -o '+shortcode+'.mp4')
+        os.system('bash yt-dlp/yt-dlp.sh --no-warnings '+ url +' -o '+shortcode+'.mp4')
         logger.info(f"File downloaded: {shortcode}.mp4")
     except Exception as e:
         logger.error(f"yt-dlp no ha descargado nada. Error {url}: {e} on line {sys.exc_info()[-1].tb_lineno} of {__file__}")
